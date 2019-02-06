ed75f56a65fc7d37229143f0e848994d69e259a5
charbel <helpmoney97@gmail.com>
2019-02-06 06:21:33 +0100

    derni-re mise à jour

--- db_config/sql_constant.py
+++ db_config/sql_constant.py
@@ -1,6 +1,6 @@
 class Constant():
     SQL = """INSERT INTO "Dashboard_equipement" (adresse_ip) VALUES (%s);"""
-    query_insert_hote = """INSERT INTO "Dashboard_equipement" (nom,adresse_ip,adresse_mac,etat) VALUES (%s,%s,%s,%s);"""
+    query_insert_hote = """INSERT INTO "Dashboard_equipement" (nom,adresse_ip,adresse_mac,etat,data) VALUES (%s,%s,%s,%s,%s);"""
     query_check_hote = """SELECT * FROM "Dashboard_equipement" WHERE adresse_mac =%s;"""
     query_insert_cpuInfo = """INSERT INTO "Dashboard_cpu_info" (cpu_number,cpu_current_freq,cpu_min_freq,cpu_max_freq,equipement_id) VALUES (%s, %s,%s, %s,%s);"""
     query_insert_ramInfo = """INSERT INTO "Dashboard_ram_info" (ram_total,ram_available,ram_percent,ram_free,ram_used,equipement_id) VALUES (%s,%s,%s,%s,%s,%s);"""
